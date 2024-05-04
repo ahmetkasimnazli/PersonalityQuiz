81eee47e6e11b561ef729539856e2c02c3130574
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-05-04 14:19:43 +0300

    commit

---
+++ PersonalityQuiz/Answer.swift
@@ -0,0 +1,8 @@
+//
+//  Answer.swift
+//  PersonalityQuiz
+//
+//  Created by Ahmet Kasım Nazlı on 3.05.2024.
+//
+
+import Foundation
---
+++ PersonalityQuiz/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalityQuiz
+//
+//  Created by Ahmet Kasım Nazlı on 3.05.2024.
+//
+
+import Foundation
